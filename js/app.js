64f385e8b717ad0f46dc839bdab7bd00a4596270
khair eldeen zakaria shkokany <shkokany98@gmail.com>
2021-05-25 23:38:34 +0300

    editing files

--- js/app.js
+++ js/app.js
@@ -105,7 +105,68 @@ default:
   alert('you are fine ?? ');
   break;
 }
-alert(score);
 
+
+
+let number=Number(prompt('choice a correct number of my anime  i loved from 1-10 '));
+number=Number(number);
+for (let i = 0; i <4; i++) {
+  console.log('type of number',typeof(number));
+  if (number === 10) {
+    alert('nice ..! , correct answear its demon slayer');
+    score++;
+    break;
+  }else if(number >10) {
+
+    number =Number(prompt('too high , guess again'));
+  }else if(number <10){
+
+    number = Number (prompt('too low , guess again'));
+  }else{
+
+    number = Number (prompt('why you didn\'t answear ..?'));
+  }
+
+  if (i===3) {
+    alert('ops ... you take all attempts :(, the answear is 10 ');
+    break;
+  }
+
+}
+
+let correctAnswer=prompt('choice a correct anime i loved :) .. ').toLowerCase();
+console.log(correctAnswer);
+let animes=['attack on titan','one piece','tokyo ghoul','demon slayer','jujutsu no kaysi'];
+let flag = false;
+for (let i = 0; i < 5; i++) {
+  for ( let j = 0; j < animes.length; j++) {
+    if (animes[j]=== correctAnswer) {
+      alert('nice ..! , correct answer its nice anime');
+      flag = true;
+      score++;
+      break;
+    } }
+  if(flag){
+    break;
+  }
+  else {
+    correctAnswer = prompt('this is not my anime , retype another one ');
+  }
+
+
+}
+let sst='';
+for(let i=0 ; i < animes.length ; i++){
+  sst = sst + ',' + animes[i];
+
+
+}
+
+alert('this is my favorite animes'+ sst);
+
+
+
+
+alert(`thanks for playing this game  ${userName} your score is ${score}`);
 
 
